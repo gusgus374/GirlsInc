600f798b561cb6069bbe0ffb73c9c4986084e05b
gusgus374 <gusalvarez374@gmail.com>
2023-10-06 12:39:55 -0400

    first commit

---
+++ FootyLab.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pathlib
+import streamlit as st
+import altair as alt
+
+st.set_page_config(
+    page_title="FootyLab",
+    page_icon="./resources/boots1.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.title("Test")
