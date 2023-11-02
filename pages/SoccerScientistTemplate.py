9592282fba28b29f1ef2db906e0e49ddca9420f6
gusgus374 <gusalvarez374@gmail.com>
2023-11-02 13:28:12 -0400

    Added subpages created by GI Soccer Scientists

---
+++ pages/SoccerScientistTemplate.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pathlib
+import streamlit as st
+import altair as alt
+
+st.title("This is a title!")
+
+st.write("Stay tuned for more examples")
